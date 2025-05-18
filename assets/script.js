19e0ce7e98cc12b40c4e015416918fc1811aaed6
wiorigami <wiorigami@qq.com>
2025-05-18 18:12:07 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,50 @@
+// 获取文件扩展名（不区分大小写）
+function getExtensionIcon(filename) {
+  const match = filename.match(/\.([^.]+)$/);
+  if (match) {
+    const ext = match[1].toLowerCase();
+    return `assets/icons/${ext}.png`;
+  }
+  return `assets/icons/default.png`;
+}
+
+function createTree(items, basePath = 'files/') {
+  const ul = document.createElement('ul');
+
+  for (const item of items) {
+    const li = document.createElement('li');
+
+    if (item.type === 'folder') {
+      const icon = document.createElement('img');
+      icon.src = 'assets/icons/folder.png';
+      icon.className = 'icon';
+
+      li.appendChild(icon);
+      li.append(' ' + item.name);
+      li.appendChild(createTree(item.children, basePath + item.name + '/'));
+    } else {
+      const icon = document.createElement('img');
+      icon.src = getExtensionIcon(item.name);
+      icon.className = 'icon';
+
+      const link = document.createElement('a');
+      link.href = basePath + item.name;
+      link.textContent = ' ' + item.name;
+      link.target = '_blank';
+
+      li.appendChild(icon);
+      li.appendChild(link);
+    }
+
+    ul.appendChild(li);
+  }
+
+  return ul;
+}
+
 fetch('assets/files.json')
   .then(res => res.json())
   .then(data => {
     const container = document.getElementById('file-tree');
-
-    function createTree(items, basePath = 'files/') {
-      const ul = document.createElement('ul');
-      for (const item of items) {
-        const li = document.createElement('li');
-        if (item.type === 'folder') {
-          li.innerHTML = `<span class="folder-icon">📁</span> ${item.name}`;
-          li.appendChild(createTree(item.children, basePath + item.name + '/'));
-        } else {
-          const link = document.createElement('a');
-          link.href = basePath + item.name;
-          link.textContent = item.name;
-          link.target = "_blank";
-          li.appendChild(link);
-        }
-        ul.appendChild(li);
-      }
-      return ul;
-    }
-
     container.appendChild(createTree(data));
   });
